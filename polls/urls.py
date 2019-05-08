61f6d9aebd3b2875d264aa7934e20d8f929de859
Unknown <f.alvaradou@alumnos.duoc.cl>
2019-05-08 16:35:33 -0300

    Comienzo pagina Admin

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index, name = "index"),
+    path('administrador',views.admin, name = "admin"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def admin(request):
+    return render(request,'admin.html')
