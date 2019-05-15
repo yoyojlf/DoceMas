34a34ec428d158c4e170b201b78e5264143ed959
Unknown <f.alvaradou@alumnos.duoc.cl>
2019-05-14 23:19:03 -0400

    Se agrega el modelo y hace la funcionalidad add
    El modelo y funcionalidad ADD está funcionando para Colegios, niveles, asignaturas y otecs.

--- polls/models.py
+++ polls/models.py
@@ -2,3 +2,34 @@ from django.db import models
 
 # Create your models here.
 
+class Asignatura(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return "ASIGNATURA"
+
+class Nivel(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return "NIVEL"
+
+class Colegio(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=10)
+    correo = models.CharField(max_length=70)
+
+    def __str__(self):
+        return "COLEGIO"
+
+class Otec(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    correo = models.CharField(max_length=70)
+    telefono = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return "OTEC"
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+#MODELOS BD
+from .models import Asignatura
+from .models import Nivel
+from .models import Otec
+from .models import Colegio
 
 # Create your views here.
 
@@ -9,16 +16,149 @@ def admin(request):
     return render(request,'admin.html')
 
 def colegio(request):
-    return render(request,'mant_colegio.html')
+    return render(request,'mant_colegio.html', {'colegios':Colegio.objects.all()} )
+
+def add_colegio(request):
+    try:
+        nom = request.POST.get('nombre','')
+        direccion = request.POST.get('direccion','')
+        correo = request.POST.get('correo','')
+        tel = request.POST.get('telefono','')
+
+        col = Colegio.objects.filter(nombre=nom)
+
+        if len(col) == 0 : 
+            col = Colegio(nombre=nom, direccion=direccion, correo=correo, telefono=tel)
+            col.save()
+            data = { 
+                'mensaje': 'El colegio fue registrado correctamente.', 
+                'type' : 'success', 
+                'tittle': 'Registro Colegio' 
+            } 
+            return JsonResponse(data) 
+
+        else:    
+            data = { 
+                'mensaje': 'Error al registrar el colegio. El colegio ya existe.', 
+                'type' : 'error', 
+                'tittle': 'Registro Colegio' 
+            } 
+            return JsonResponse(data)
+    except:
+        data = { 
+            'mensaje': 'Error al agregar el colegio.', 
+            'type' : 'error', 
+            'tittle': 'Registro Colegio' 
+        } 
+        return JsonResponse(data)
 
 def asig(request):
-    return render(request,'mant_asignatura.html')
+    return render(request,'mant_asignatura.html', {'asignaturas':Asignatura.objects.all()} )
+
+def add_asig(request):
+
+    try:
+        nom = request.POST.get('nombre','')
+        descrip = request.POST.get('descripcion','')
+
+        asig = Asignatura.objects.filter(nombre=nom)
+
+        if len(asig) == 0 : 
+            asig = Asignatura(nombre=nom, descripcion=descrip)
+            asig.save()
+            data = { 
+                'mensaje': 'La asignatura fue registrada correctamente.', 
+                'type' : 'success', 
+                'tittle': 'Registro Asignatura' 
+            } 
+            return JsonResponse(data) 
+
+        else:    
+            data = { 
+                'mensaje': 'Error al registrar la asignatura. La asignatura ya existe.', 
+                'type' : 'error', 
+                'tittle': 'Registro Asignatura' 
+            } 
+            return JsonResponse(data)
+    except:
+        data = { 
+            'mensaje': 'Error al agregar asignatura.', 
+            'type' : 'error', 
+            'tittle': 'Registro Asignatura' 
+        } 
+        return JsonResponse(data)
     
 def nivel(request):
-    return render(request,'mant_nivel.html')
+    return render(request,'mant_nivel.html', {'niveles':Nivel.objects.all()} )
+
+def add_nivel(request):
+    try:
+        nom = request.POST.get('nombre','')
+        descrip = request.POST.get('descripcion','')
+
+        nivel = Nivel.objects.filter(nombre=nom)
+
+        if len(nivel) == 0 : 
+            nivel = Nivel(nombre=nom, descripcion=descrip)
+            nivel.save()
+            data = { 
+                'mensaje': 'El nivel fue registrado correctamente.', 
+                'type' : 'success', 
+                'tittle': 'Registro Nivel' 
+            } 
+            return JsonResponse(data) 
+
+        else:    
+            data = { 
+                'mensaje': 'Error al registrar el nivel. El nivel ya existe.', 
+                'type' : 'error', 
+                'tittle': 'Registro Nivel' 
+            } 
+            return JsonResponse(data)
+    except:
+        data = { 
+            'mensaje': 'Error al agregar el nivel.', 
+            'type' : 'error', 
+            'tittle': 'Registro Nivel' 
+        } 
+        return JsonResponse(data)
 
 def usuario(request):
     return render(request,'mant_usuario.html')
 
 def otec(request):
-    return render(request,'mant_otec.html')
+    return render(request,'mant_otec.html', {'otecs':Otec.objects.all()} )
+
+def add_otec(request):
+    try:
+        nom = request.POST.get('nombre','')
+        direccion = request.POST.get('direccion','')
+        correo = request.POST.get('correo','')
+        tel = request.POST.get('telefono','')
+
+        otec = Otec.objects.filter(nombre=nom)
+
+        if len(otec) == 0 : 
+            otec = Otec(nombre=nom, direccion=direccion, correo=correo, telefono=tel)
+            otec.save()
+            data = { 
+                'mensaje': 'La Otec fue registrada correctamente.', 
+                'type' : 'success', 
+                'tittle': 'Registro Otec' 
+            } 
+            return JsonResponse(data) 
+
+        else:    
+            data = { 
+                'mensaje': 'Error al registrar la Otec. La Otec ya existe.', 
+                'type' : 'error', 
+                'tittle': 'Registro Otec' 
+            } 
+            return JsonResponse(data)
+    except:
+        data = { 
+            'mensaje': 'Error al agregar la Otec.', 
+            'type' : 'error', 
+            'tittle': 'Registro Otec' 
+        } 
+        return JsonResponse(data)
