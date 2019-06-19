d527bba4781af631ff47d5d8303feb602eb72b1f
Yoyo DELL Station <jordhan_07@hotmail.com>
2019-06-19 10:24:54 -0400

    Se agrega script de intalacion de las dependencias

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo Instalando los aplicativos necesarios para el funcionamiento de la APP
+
+@echo Actualizando python
+python -m pip install --upgrade pip
+
+@echo Instalando Cripsy-forms
+pip install --upgrade django-crispy-forms
+
+@echo Instalación completa...
+pause
